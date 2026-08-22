78b179eeef88a87b528514085a47c53544b9d598
yukthesh <yuktheshy@gmail.com>
2026-08-22 18:07:29 +0530

    Delete Day 13 directory

--- Day 13/assert.py
+++
@@ -1,8 +0,0 @@
-email = 'sdfgh'
-password = "5849"
-
-amount = 20000
-
-assert amount > 0, "amount needs to be +ve"
-
-assert email!='' and password!='','userneeds to give email and pwd'
--- Day 13/pass.py
+++
@@ -1,6 +0,0 @@
-if 10 == 10:
-    pass
-for i in range(1,10):
-    pass
-def verify():
-    pass
